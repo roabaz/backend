cc2710b46d13368ae0cbc66e45d75ddd87346940
roabaz <robazter@gmail.com>
2022-12-23 12:33:22 -0300

    rutas, metodos, server

--- dasafio3/src/app.js
+++ dasafio3/src/app.js
@@ -1,26 +1,11 @@
 import express from "express";
 const app = express();
 
-import { createRequire } from "module"; // Bring in the ability to create the 'require' method
-const require = createRequire(import.meta.url); // construct the require method
-const products = require("../../desafio2/products.json") // use the json
 
-app.get("/products", (req, res) => {
-  if(req.query.limit){
-    res.json(products.slice(req.query.start || 0,req.query.limit));
-  }else{
-    res.json(products);
-  }
-  });
 
-app.get("/products/:id", (req, res) => {
-  const product = products.find((u) => (u.id === parseInt(req.params.id)));
-  if (product) {
-    res.json(product);
-  } else {
-    res.status(404).json({ error: "producto no encontrado" });
-  }
-});
+
+
+
 
 app.listen(8080, () => {
   console.log("Servidor arriba en puerto 8080",`http://localhost:8080/products`);
