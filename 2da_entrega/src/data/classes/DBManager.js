c4167868a9feb7adb3a9437821b751e34769f395
roabaz <robazter@gmail.com>
2023-02-10 20:06:38 -0300

    add productToCart remove one an all product

--- 2da_entrega/src/data/classes/DBManager.js
+++ 2da_entrega/src/data/classes/DBManager.js
@@ -140,7 +140,53 @@ class CartFileManager {
       throw err;
     }
   }
+
+
+  async addProductToCart(cid, pid, quantity){
+        try{
+            const cartId = await cartModel.findById(cid);
+            let productId =  cartId.products.find(p => p.product.toString() === pid.toString())
+            if (productId){
+               productId.quantity = quantity
+            }else{
+                cartId.products.push({product:pid, quantity:quantity});
+            }
+            const cartUpdate = await cartModel.updateOne({_id: cid},cartId)
+            return cartUpdate
+        }
+        catch (err) {
+            throw err;
+        }
+    }
+
+    async removeProductFromCart(cid, pid){
+        try{
+            const cartId = await cartModel.findById(cid);
+                const findproduct = cartId.products
+                const productCart = findproduct.findIndex(p=> p.product.toString() === pid.toString())
+                
+                findproduct.splice(productCart,1)
+                const update = {products: findproduct}
+                const updateCart = await cartModel.findByIdAndUpdate(cid, update );
+                return updateCart
+        }catch (err) {
+            throw err;
+        }
+    }
+
+    async deleteAllProductCart(id){
+        try {
+            const deleteProduct = {products:[]}
+            const cart = await cartModel.findByIdAndUpdate(id, deleteProduct);
+            return cart;
+          } catch (err) {
+            throw err;
+          }
+    }
 }
+
+
+
 
 
 
